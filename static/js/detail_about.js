2f21df022765f0f8940fb505aaa9c85f01251730
Lopes, Maycon <lopes.maycon@gmail.com>
2022-08-18 11:13:48 -0300

    Button 'Saiba Mais' now also erase the about title.

--- static/js/detail_about.js
+++ static/js/detail_about.js
@@ -1,14 +1,18 @@
 const simple_about = document.querySelector('.simple-about-text');
 const more_about = document.querySelector('.more-about-text');
 const change_btn = document.querySelector('.change-btn');
+const about_title = document.getElementById('about-title')
+const about_title_bkp = about_title.innerText;
 
 change_btn.addEventListener('click', () => {
     simple_about.classList.toggle('hidden');
     more_about.classList.toggle('hidden');
     if(change_btn.innerText === 'Saiba mais'){
         change_btn.innerText = 'Menos';
+        about_title.innerText = ''
     }else{
         change_btn.innerText = 'Saiba mais';
+        about_title.innerText = about_title_bkp;
     }
     window.location.href = '#about-me';
 
