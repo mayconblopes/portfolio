be2013b4b6a6b7941944d5d67f556c5f8a9e5a52
Lopes, Maycon <lopes.maycon@gmail.com>
2022-08-20 06:15:53 -0300

    Implemented paginator for Blog ListView

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,11 @@ from blog.models import BlogPost
 
 class BlogPubPostListView(ListView):
     model = BlogPost
+    paginate_by = 8
     template_name = 'blog_index.html'
     queryset = BlogPost.objects.all().filter(is_draft=False)
     context_object_name = 'list_of_pub_posts'
+
 
 
 class BlogPubPostDetailView(DetailView):
