8496eb11a3b364df7091a5e246c138cfc221700d
Lopes, Maycon <lopes.maycon@gmail.com>
2022-08-20 08:31:14 -0300

    Preparing to deploy

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -20,8 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SK')
+#SECRET_KEY
+if os.getenv('SECRET_KEY'):
+    SECRET_KEY = os.getenv('SECRET_KEY')
+else:
+    SECRET_KEY = config('SK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
