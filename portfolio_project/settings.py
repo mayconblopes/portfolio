63940238ef65850f0a9e7abfab89f9a6725e1543
Lopes, Maycon <lopes.maycon@gmail.com>
2022-08-22 22:57:38 -0300

    Investiganting error 403 when trying to login to admin...

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -142,3 +142,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+# Resolves CSRF problem with HTTPS. See more https://docs.djangoproject.com/en/dev/releases/4.0/#format-change
+CSRF_TRUSTED_ORIGINS = ['https://www.mayconblopes.ml']
